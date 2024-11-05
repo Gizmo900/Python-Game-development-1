77d34e78a4c56ef684023c52efbd30acf1091dd6
Gizmo900 <jahromia75@gmail.com>
2024-11-05 17:40:20 +0000

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,45 @@
+dep = ("dog",)
+print(type(dep))
+dep = ("dog",5,"red")
+my_list = [5 ,7,5 ,"hi"]
+#typecasting
+change_Tuple = tuple(my_list)
+print (change_Tuple)
+print(change_Tuple[3])
+
+#tuple is immutable
+#packing
+address = (78 , "maple road","London",560934)
+dno, road,city,zipcode = address
+print(zipcode)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
